24ed44926cd7ce8ae4c441c68e473ccdbbbbf4da
keysRB <keysrb@gmail.com>
2014-10-29 21:58:41 -0400

    Streamlined the code
    Implemented a for loop while trying to learn the use of methods.

--- src/LetterToYourself.java
+++ src/LetterToYourself.java
@@ -2,47 +2,24 @@
 
 public class LetterToYourself {
 
-	public static void main(String[] args) {
-		String horizLine = "+---------------------------------------------------------+"; // Form top & bottom
-		//int formatCheck = 0; 
-		int lineCount = 0; //Count number of lines.
-
-
-		
-		while (lineCount < 10) { //Check to see if box should be finished.
-			if (lineCount == 0) {
-				System.out.println(horizLine); //Print Top Format Line
-				lineCount++;
-			} else if (lineCount == 1) {
-				System.out.println("|						     #### |");
-				lineCount++;
-			} else if (lineCount == 2) {
-				System.out.println("|						     #### |");
-				lineCount++;
-			} else if (lineCount == 3) {
-				System.out.println("|						     #### |");
-				lineCount++;
-			} else if (lineCount == 4) {
-				System.out.println("|						          |");
-				lineCount++;
-			} else if (lineCount == 5) {
-				System.out.println("|						          |");
-				lineCount++;
-			} else if (lineCount == 6) {
-				System.out.println("|			KEYS RB               		  |");
-				lineCount++;
-			} else if (lineCount == 7) {
-				System.out.println("|			1 Address Way         		  |");
-				lineCount++;
-			} else if (lineCount == 8) {
-				System.out.println("|			New York, NY 12345    		  |");
-				lineCount++;
-			} else if (lineCount == 9) {
-				System.out.println("|						          |");
-				System.out.println(horizLine);
-				lineCount++;
-			}
-		}
+	public static void main(String[] args) {		
+		printLetter(); //Call to execute printing method.
 	}
 
+	private static void printLetter() { //Display letter with information
+		String horizLine = "+---------------------------------------------------------+"; // Form top & bottom
+		for (int i = 0; i<1; i++ ){
+			System.out.println(horizLine); //Print Top Format Line
+			System.out.println("|						     #### |");
+			System.out.println("|						     #### |");
+			System.out.println("|						     #### |");
+			System.out.println("|						          |");
+			System.out.println("|						          |");
+			System.out.println("|			KEYS RB               		  |");
+			System.out.println("|			1 Address Way         		  |");
+			System.out.println("|			New York, NY 12345    		  |");
+			System.out.println("|						          |");
+			System.out.println(horizLine);
+		}
+	}
 }

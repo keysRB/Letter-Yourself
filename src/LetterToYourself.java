494c5bc5f44dd24c7182afd3e800db5f66eaf25d
keysRB <keysrb@gmail.com>
2014-10-31 12:01:55 -0400

    User input accepted
    Added the ability for the user to add their own personal information.
    Need to keep the envelope's formatting regardless of user information
    entered.
    
    Need to work on getting the envelope to line up exactly, has to do with
    "int" rounding leaving the finishing | to be one short in some cases.

--- src/LetterToYourself.java
+++ src/LetterToYourself.java
@@ -1,24 +1,75 @@
 //Purpose: Print a letter to yourself stating your name and address. Formatting should be included around info.
+import java.util.Scanner;
 
 public class LetterToYourself {
 
-	public static void main(String[] args) {		
-		printLetter(); //Call to execute printing method.
-	}
-
-	private static void printLetter() { //Display letter with information
-		String horizLine = "+---------------------------------------------------------+"; // Form top & bottom
+	public static void main(String[] args) {
+		
+		Scanner userInput = new Scanner(System.in);
+		System.out.printf("Please enter your first name: ");
+		String firstName = userInput.nextLine();
+		System.out.printf("Please enter your last name:");
+		String lastName = userInput.nextLine();
+		System.out.printf("Please enter your address:");
+		String userAddress = userInput.nextLine();
+		System.out.printf("Please enter your City, State Zip Code:");
+		String userLocation = userInput.nextLine();
+		
+		String horizLine = "+------------------------------------------------------------+"; // Form top & bottom
 		for (int i = 0; i<1; i++ ){
 			System.out.println(horizLine); //Print Top Format Line
-			System.out.println("|						     #### |");
-			System.out.println("|						     #### |");
-			System.out.println("|						     #### |");
-			System.out.println("|						          |");
-			System.out.println("|						          |");
-			System.out.println("|			KEYS RB               		  |");
-			System.out.println("|			1 Address Way         		  |");
-			System.out.println("|			New York, NY 12345    		  |");
-			System.out.println("|						          |");
+			System.out.println("|						        #### |");
+			System.out.println("|						        #### |");
+			System.out.println("|						        #### |");
+			System.out.println("|						             |");
+			System.out.println("|						             |");
+		
+			// *******Format name line with information to fit the box formatting: ********************
+			System.out.printf("|");
+			int nameLength = firstName.length() + lastName.length() + 1; // Add 1 for space between names
+			int totalSpace = (60 - nameLength);
+			int spaceLeft = totalSpace / 2;
+			for (int j = 0; j<spaceLeft; j++ ) {  //Need to count number of spaces to print before and after name
+				System.out.printf(" ");
+			}
+			System.out.printf(firstName + " " + lastName);
+			for (int k = 0; k < spaceLeft; k++) {
+				System.out.printf(" ");
+			}
+			System.out.println("|");
+			// ****************************************************************************************
+			
+			// *******Format address line with information to fit the box formatting: ********************
+			System.out.printf("|");
+			int addressLength = userAddress.length(); // Find length of address
+			totalSpace = (60 - addressLength);
+			spaceLeft = totalSpace / 2;
+			for (int j = 0; j<spaceLeft; j++ ) {  //Need to count number of spaces to print before and after name
+				System.out.printf(" ");
+			}
+			System.out.printf(userAddress);
+			for (int k = 0; k < spaceLeft; k++) {
+				System.out.printf(" ");
+			}
+			System.out.println("|");
+			// ****************************************************************************************
+			
+			// *******Format location line with information to fit the box formatting: ********************
+			System.out.printf("|");
+			int locationLength = userLocation.length(); // Find length of address
+			totalSpace = (60 - locationLength);
+			spaceLeft = totalSpace / 2;
+			for (int j = 0; j<spaceLeft; j++ ) {  //Need to count number of spaces to print before and after name
+				System.out.printf(" ");
+			}
+			System.out.printf(userLocation);
+			for (int k = 0; k < spaceLeft; k++) {
+				System.out.printf(" ");
+			}
+			System.out.println("|");
+			// ****************************************************************************************
+			
+			System.out.println("|						             |");
 			System.out.println(horizLine);
 		}
 	}
